6490954b7703ce471f3d3727e85256d3e3f4c856
Mauduit-O <oum-el-khaire.mauduit@hetic.net>
2020-06-16 22:18:37 +0200

    add viedo karanga in view karanga

--- src/views/Karanga.vue
+++ src/views/Karanga.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="karanga" id="karanga">
     <Muted />
-    <ArticleImg :title="title" :description="description" />
+    <ArticleVideo :video="video" />
+    <ArticleImg :title="title" :description="description"></ArticleImg>
+
     <TimeLine />
   </div>
 </template>
@@ -13,7 +15,7 @@ export default {
       title: "karanga",
       description:
         "Le Karanga, qui signifie appeler, invoquer, est une des étapes de la cérémonie du powhiri. C'est un échange d'appels chantés presque exclusivement par les femmes dans la langue maorie, le Reo Maori. Il a lieu lorsqu'un groupe de visiteurs se déplace sur le marae ou dans la zone de réunion officielle, dans le but de rendre hommage aux morts et de se référer à la raison de la réunion des groupes.",
-
+      video: require("@/assets/video/video-karanga.mp4"),
       scrollTop: 1,
     };
   },
@@ -48,10 +50,10 @@ export default {
 
 <style lang="scss" scoped>
 @import "@/assets/css/styles.scss";
-.karanga {
-  background-image: url(../assets/img/backgroundKaranga.png);
-  @include backgroundImg(no-repeat, cover, top center);
-  width: 100vw;
-  height: 100vh;
-}
+// .karanga {
+//   background-image: url(../assets/img/backgroundKaranga.png);
+//   @include backgroundImg(no-repeat, cover, top center);
+//   width: 100vw;
+//   height: 100vh;
+// }
 </style>
